f46ecb5f47856f9fd28ef3120e4c764c1dd2834c
Muniba26 <muniba_scorpio@yahoo.com>
2026-03-25 19:41:53 +0400

    Add breadcrumb schema homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -140,6 +140,23 @@ export default function HomePage() {
     }),
   }}
 />
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: "Home",
+          item: "https://www.fbacalculatoruae.com",
+        },
+      ],
+    }),
+  }}
+/>
     </main>
   );
 }
